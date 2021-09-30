3922937fabbfce03e70df79cf9819ad733e0010b
c0repwn3r <36366790+c0repwn3r@users.noreply.github.com>
2021-09-30 11:29:46 -0400

    Update loader.js

--- loader.js
+++ loader.js
@@ -15,5 +15,30 @@
  * -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
  */
  
- (function (w) {
- 
+(function (w) {
+    w.bionixIsBlacklisted = function (url, s) {
+        // check if its a blacklisted url
+        if (s) { return false };
+        blacklistedUrls = ["https://coresdev.ml/bad.js"];
+        if (blacklistedUrls.includes(url)) {
+            return true;
+        }
+        return false;
+    }
+    w.bionixScanUrlScript = function (url, s) {
+        if (s) { return false };
+        // malware scanning not implemented
+        return false;
+    }
+    w.bionixInject = function (url, s) {
+        // inject script from url
+        if (w.bionixIsBlacklisted(url, s)) {
+            return false;
+        }
+        // script isnt on blacklist, run antimalware scan
+        if (w.bionixScanUrlScript(url, s)) {
+            return false;
+        }
+        // clear to execute
+    }
+})(window);
