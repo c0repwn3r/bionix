379cb17b85a3419c6cf7428be34526e6ba76f84c
c0repwn3r <36366790+c0repwn3r@users.noreply.github.com>
2021-09-30 11:22:17 -0400

    Create loader.js

---
+++ loader.js
@@ -0,0 +1,19 @@
+/*
+ *      __                           ____    
+ *  __ /  \ _ _ ___ _ ____ __ ___ _ |__ /_ _ 
+ * / _| () | '_/ -_) '_ \ V  V / ' \ |_ \ '_|
+ * \__|\__/|_| \___| .__/\_/\_/|_||_|___/_|  
+ *                   |_|                       
+ *
+ * Please do not steal my code, I will just be mad at you. Learn it yourself.
+ * -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+ * 
+ * File: loader.js
+ * Description: Background scripts and general utilities that bionix uses to inject scripts
+ * Owner: gh/c0repwn3r/c0repwn3r
+ * Project: bionix
+ * -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+ */
+ 
+ (function (w) {
+ 
